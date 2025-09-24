2d976fe5f0771c93e7cd8ad46baf1d5eee502e5c
Pritesh Kakani <priteshgkakani@gmail.com>
2025-09-24 12:08:36 +0530

    build: fix tailwind postcss plugin and Vite svg import; prod build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+const viteLogo = '/vite.svg'
 import { setupCounter } from './counter.js'
 import { setupNifty } from './nifty.js'
 
